986ebc60e3d6444f9061213aee1f09b32928baf5
FAHAD <eng.bokhari@hotmail.com>
2024-04-20 20:58:24 +1200

    Add files via upload
    Update on this var: 
    - Extract data from GET request.
    - Store data into data frame.
    - Export data to CSV file.

--- webscrapping.py
+++ webscrapping.py
@@ -11,6 +11,7 @@ def extract_column_from_header(table):
     This function returns the landing status from the HTML table cell 
     Input: the  element of a table data cell extracts extra row
     """
+    wrong_columns = ['\n','1\n','2\n','3\n','4\n','5\n','6\n','7\n']
     columns = []
     for i,row in enumerate(table):
         if (row.br):
@@ -26,7 +27,7 @@ def extract_column_from_header(table):
         if not(colunm_name.strip().isdigit()):
             colunm_name = colunm_name.strip()
         
-        if colunm_name != None and colunm_name != '':
+        if colunm_name != None and colunm_name != '' and colunm_name not in wrong_columns:
             columns.append(colunm_name)
 
     return columns
@@ -52,22 +53,20 @@ def create_df(col_names, soap):
             if flag:
                 extracted_row += 1
                 # Flight Number value
-                # TODO: Append the flight_number into launch_dict with key `Flight No.`
-                #print(flight_number)
-                
-                launch_dict['Flight No.'] = flight_number
+                # TODO: Append the flight_number into launch_dict with key `Flight No.`                
+                launch_dict['Flight No.'].append(flight_number) 
 
                 # Date value
                 # TODO: Append the date into launch_dict with key `Date`
                 datatimelist=date_time(row[0])
                 date = datatimelist[0].strip(',')
-                launch_dict['Date'] = date
+                launch_dict['Date'].append(date)
                 #print(date)
                 
                 # Time value
                 # TODO: Append the time into launch_dict with key `Time`
                 time = datatimelist[1]
-                launch_dict["Time"] = [time]
+                launch_dict["Time"].append(time)
                 #print(time)
                 
                 # Booster version
@@ -75,50 +74,46 @@ def create_df(col_names, soap):
                 bv=booster_version(row[1])
                 if not(bv):
                     bv=row[1].a.string
-                launch_dict['Version Booster'] = bv
+                launch_dict['Version Booster'].append(bv)
                 #print(bv)
                 
                 # Launch Site
                 # TODO: Append the bv into launch_dict with key `Launch Site`
                 launch_site = row[2].a.string
-                launch_dict['Launch site'] = launch_site
+                launch_dict['Launch site'].append(launch_site)
                 #print(launch_site)
                 
                 # Payload
                 # TODO: Append the payload into launch_dict with key `Payload`
                 payload = row[3].a.string
-                launch_dict['Payload'] = payload
+                launch_dict['Payload'].append(payload)
                 #print(payload)
                 
                 # Payload Mass
                 # TODO: Append the payload_mass into launch_dict with key `Payload mass`
                 payload_mass = get_mass(row[4])
-                launch_dict['Payload mass'] = payload_mass
+                launch_dict['Payload mass'].append(payload_mass)
                 #print(payload)
                 
                 # Orbit
-                # TODO: Append the orbit into launch_dict with key `Orbit`
                 orbit = row[5].a.string
-                launch_dict['Orbit'] = orbit
+                launch_dict['Orbit'].append(orbit)
                 #print(orbit)
                 
                 # Customer
-                # TODO: Append the customer into launch_dict with key `Customer`
-                if type(row[6]) == 'bs4.element.Tag':
+                #if type(row[6]) == 'bs4.element.Tag':
+                if row[6].a:
                     customer = row[6].a.string
-                    launch_dict['Customer'] = customer
+                    launch_dict['Customer'].append(customer)
 
                 # Launch outcome
-                # TODO: Append the launch_outcome into launch_dict with key `Launch outcome`
                 launch_outcome = list(row[7].strings)[0]
-                launch_dict['Launch outcome'] = launch_outcome
-                #print(launch_outcome)
+                launch_dict['Launch outcome'].append(launch_outcome)
                 
                 # Booster landing
-                # TODO: Append the launch_outcome into launch_dict with key `Booster landing`
                 booster_landing = landing_status(row[8])
-                launch_dict['Booster landing'] = booster_landing
-                #print(booster_landing)
+                launch_dict['Booster landing'].append(booster_landing)
+    return launch_dict
 
 def create_dict_(column_names):
     launch_dict= dict.fromkeys(column_names)
@@ -184,17 +179,16 @@ def main():
     
     # Obtain table columns
     column_names = extract_column_from_header(first_lnch_table)
-    #for i,row in enumerate(first_lnch_table):
-    #    col_name = extract_column_from_header(row)
-        
-    #    if col_name != None and col_name != '':
-    #        column_names.append(col_name)
-
 
     # Store data returned into dataframe 
-    create_df(column_names, soap)
+    lunch_dict = create_df(column_names, soap)
 
+    # Create datafrmae that 
+    df= pd.DataFrame({ key:pd.Series(value) for key, value in lunch_dict.items() })
+    df.head()
 
+    # Export data into CSV file 
+    df.to_csv('webscrapping_data.csv', index=False)
 if __name__ == '__main__':
   main()
 
